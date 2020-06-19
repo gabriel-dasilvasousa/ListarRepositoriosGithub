e833d68f44f63f07127be533a459b32e04693c9d
gabriel-dasilvasousa <g.silva13455@gmail.com>
2020-06-19 13:36:14 -0300

    Versão funcional

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
+import api from './api';
+
 class App{
     constructor() {
         this.repositories = [];
 
         this.formEl = document.getElementById('repo-form');
+        this.inputEl = document.querySelector('input[name=repository]');
         this.listEl = document.getElementById('repo-list');
 
         this.registerHandlers();
@@ -12,16 +15,50 @@ class App{
         this.formEl.onsubmit = event => this.addRepository(event);
     }
 
-    addRepository(event){
-        event.preventDefault();
-        this.repositories.push({
-            name: 'Gabriel da Silva Sousa',
-            description: 'Irineu vc nao sabe nem eu',
-            avatar_url: 'https://avatars3.githubusercontent.com/u/54194379?s=460&u=3a7c3b0f100ec50ab019e88af47f0e71b02efe24&v=4',
-            html_url: 'https://github.com/gabriel-dasilvasousa',
-        });
+    setLoading(loading = true) {
+        if(loading === true) {
+            let loadingEl = document.createElement('span');
+            loadingEl.appendChild(document.createTextNode('Carregando'));
+            loadingEl.setAttribute('id', 'loading');
 
-        this.render();
+            this.formEl.appendChild(loadingEl);
+        }
+        else {
+            document.getElementById('loading').remove();
+        }
+    }
+
+    async addRepository(event){
+        event.preventDefault();
+
+        const repoInput = this.inputEl.value;
+
+        if(repoInput.length === 0){
+            return;
+        }
+
+        this.setLoading();
+
+        try{
+            const response = await api.get(`/users/${repoInput}`);
+
+            const { name, bio, html_url, avatar_url } = response.data;
+
+            this.repositories.push({
+                name,
+                bio,
+                avatar_url,
+                html_url,
+            });
+
+            this.render();
+        }
+        catch(err) {
+            alert("O repositório não existe");
+        }
+        finally{
+            this.setLoading(false);
+        }
     }
 
     render() {
@@ -35,7 +72,7 @@ class App{
             titleEl.appendChild(document.createTextNode(repository.name));
 
             let descriptionEl = document.createElement('p');
-            descriptionEl.appendChild(document.createTextNode(repository.description));
+            descriptionEl.appendChild(document.createTextNode(repository.bio));
 
             let linkEl = document.createElement('a')
             linkEl.setAttribute('target', 'blank');
